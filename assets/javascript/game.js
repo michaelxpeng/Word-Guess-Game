fd06c2c46629d03b36f1e92b735bff4dc2f618b8
mpeng <michaelxpeng@gmail.com>
2018-06-07 02:27:01 -0400

    basic pseudo codes

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+//Create an array of All-Stars
+
+// Create variables for other wins counter, loss counter, guesses left counter
+
+// Create empty arrays for computer's choice, underscores, guessed letters and incorrect guesses
+
+// Create a boolean variable to check if the game is running
+
+// Create variables that refer to DOM elements
+
+// Create a function that starts/resets the game with starting values assigned
+
+// Make computer randomly pick a name from the array of All-Stars
+
+// Create corresponding amount of underscores to the selected name
+
+// Send the information to DOM
+
+// Create an event listener that takes starts the game when user clicks on the start button
+
+// Create an event that takes user's guesses based on key entered
+
+// Check to see if the guessed letter matches any of the 
+
+// Check to see if the guessed letter is in computer's chosen name
+
+// If so, swap out the corresponding underscore with the correct letter
+
+// If the user tries to guess a letter already guessed, send an alert asking the user to pick a different letter
+
+// Send the information to DOM
+
+// If the user's guess is incorrect, amount of guesses decreases, the incorrect letter is added to the list of incorrect guesses
+
+// Run a function to see if the user wins or loses
+
+// Loss happens when the guesses left reaches 0
+
+// If it's a loss, then end the game, add 1 to the loss counter, alert the user he/she lost, and show computer's choice
+
+// If it's a win, then end the game, add 1 to the win counter and alert the user he/she won
